f6c89d947bd74c998cc84ac3c739d7ea9cabd47c
bencelder <ben@yukawa.local>
2015-10-18 12:56:44 -0400

    started cleanup

--- sea.js
+++ sea.js
@@ -1,11 +1,7 @@
 function initialize(){
     c = document.getElementById("myCanvas");
     ctx = c.getContext("2d");
-    c.style.cursor = "none";
 
-    //cursor = new Cursor(0, 0);
-
-    paused = -1; // -1 = not paused, 1 = paused
     keys = [];
 
     x_min = -2.;
@@ -15,24 +11,7 @@ function initialize(){
 
     dx = (y_max - y_min) / c.height;
 
-    //dy = (y_max - y_min)/c.height;
-
-    /*
-    var z = new Complex(0, 0);
-    var c = new Complex(0.25, 0.5);
-
-    for (var i = 0; i < 10; i++){
-        z = P_next(z, c);
-        c_print( z );
-    }
-    */
-
     draw_fract();
-
-    //music.play();
-
-    //lastframe = Date.now();
-    //game_loop = setInterval( function(){loop()}, 1);
 }
 
 function P_next(z, c){
@@ -54,7 +33,7 @@ function draw_fract(){
             var z = new Complex(0, 0);
             var b = new Complex(x, y);
 
-            for (var k = 0; k < 10; k++)
+            for (var k = 0; k < 50; k++)
                 z = P_next(z, b);
 
             if (c_mag(z) < 2.)
@@ -100,70 +79,15 @@ function c_mag(a){
     return out;
 }
 
-function diverges(x, y){
-    var c = new Complex(x, y);
-
-    var n = steps_to_diverge(c);
-
-    if (n == -1.)
-        return false;
-    return true;
-
-}
-
-function steps_to_diverge(c){
-    var z = new Complex(0, 0);
-
-    for (var n = 0; n < 100; n++){
-        if (c_mag(z) >= 2.)
-            return n;
-
-        // z -> z^2 + c
-        z = c_square(z);
-        a = c_add(z, c);
-    }
-    return -1.;
-}
-
-function loop(){
-    now = Date.now();
-    dt = (now - lastframe)/1000;
-    lastframe = now;
-
-    // update game objects
-
-    // check for destruction
-
-    // draw everything
-    //draw();
-
-}
-
-function draw(){
-    ctx.fillStyle = "#002447";
-    ctx.fillRect(0, 0, c.width, c.height);
-
-    cursor.draw();
-    player.draw();
-
-    for (var i = 0; i < bullets.length; i++)
-        bullets[i].draw();
-
-    for (var i = 0; i < patrols.length; i++)
-        patrols[i].draw();
-}
-
 
 function keyDown(e){
-    //alert(e.keyCode);
     kc = e.keyCode;
+    //alert(e.keyCode);
     keys[kc] = true;
-
-    if (kc == 80)
-        paused *= -1;
 }
 
 function keyUp(e){
+    kc = e.keyCode;
     keys[e.keyCode] = false;
 }
 
